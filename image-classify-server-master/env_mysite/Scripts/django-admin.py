8adca3ada7301144232483cd12fdbd5fd8b7a1d5
LuckyRathod <luckyrathod46@gmail.com>
2019-06-04 19:10:15 +0530

    Csp App
    Final Commit

---
+++ image-classify-server-master/env_mysite/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\adity\desktop\image-classify-server-master\env_mysite\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
